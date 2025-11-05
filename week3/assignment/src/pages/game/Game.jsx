1237ea25bcaccb7ab2698d28d1fe33d1ad188380
jstar_00 <wltjd6300@naver.com>
2025-11-05 21:56:18 +0900

    feat: useShuffle 커스텀 훅 생성 및 Game, GameBoard에 적용

--- week3/assignment/src/pages/game/Game.jsx
+++ week3/assignment/src/pages/game/Game.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import * as styles from "./Game.css";
 import GameBoard from "./components/game-board/GameBoard";
 import GameInfo from "./components/game-info/GameInfo";
+import { useShuffle } from "./hooks/useShuffle";
 
 const Game = () => {
+  const { deck, level, gridSize, resetDeck, changeLevel } = useShuffle(1);
+
   return (
     <main className={styles.container}>
-      <GameBoard gridSize={4} />
-      <GameInfo />
+      <GameBoard
+        deck={deck}
+        gridSize={gridSize}
+        resetDeck={resetDeck}
+      />
+      <GameInfo
+        level={level}
+        changeLevel={changeLevel}
+      />
     </main>
   );
 };
--- week3/assignment/src/pages/game/components/game-board/GameBoard.jsx
+++ week3/assignment/src/pages/game/components/game-board/GameBoard.jsx
@@ -1,24 +1,22 @@
 import React from 'react'
 import * as styles from './GameBoard.css'
 
-const GameBoard = ({ gridSize = 4 }) => {
-  // gridSize x gridSize 만큼의 카드 생성
-  const totalCards = gridSize * gridSize;
-  const cards = Array.from({ length: totalCards }, (_, index) => index);
-
+const GameBoard = ({ deck, gridSize, resetDeck }) => {
   return (
     <div className={styles.container}>
       <div className={styles.header}>
         <h2 className={styles.headerTitle}>게임 보드</h2>
-        <button type="button" className={styles.resetButton}>게임 리셋</button>
+        <button type="button" className={styles.resetButton} onClick={resetDeck}>
+          게임 리셋
+        </button>
       </div>
 
       <div
         className={styles.boardGrid}
-        style={{ gridTemplateColumns: `repeat(${gridSize}, 1fr)` }}
+        style={{ gridTemplateColumns: `repeat(${gridSize.cols}, 1fr)` }}
       >
-        {cards.map((index) => (
-          <div key={index} className={styles.card}>
+        {deck.map((card) => (
+          <div key={card.id} className={styles.card}>
             ?
           </div>
         ))}
