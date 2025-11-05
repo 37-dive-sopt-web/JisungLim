6eb77d90d58947ba464ff83d1668930a3335daa8
jstar_00 <wltjd6300@naver.com>
2025-11-05 18:57:32 +0900

    setting: Pretendard 설치(Variable Font)

--- week3/assignment/src/main.jsx
+++ week3/assignment/src/main.jsx
@@ -1,9 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "pretendard/dist/web/variable/pretendardvariable.css"; // Pretendard Variable Font
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
